f3b862c404fb0d78a201b1564981aa1b9bb1fad5
CowieBunga <jackson.cowie@greenwoodcollege.com>
2019-02-13 11:04:14 -0500

    test

---
+++ 1.1 File I/Day 1 Unit 2.py
@@ -0,0 +1,29 @@
+
+'''
+dic = {"year": 2019, "Car": "Honda Civic"}
+print(dic)
+'''
+
+
+'''
+dic = {"vegetable": "lettuce", "fruit": "mango"}
+del dic["fruit"]
+'''
+
+'''
+dic = {}
+dic["food"] = "hamburger"
+dic["drink"] = "chocolate milk"
+dic["side plate"] = "mac and cheese"
+'''
+
+'''
+dic = {"apple": "macintosh", "ape": "chimpanzee", "jam": "strawberry"}
+list = []
+for key in dic:
+    if key[0] == 'a':
+        list.append(key)
+for value in list:
+    del dic[value]
+print(dic)
+'''
